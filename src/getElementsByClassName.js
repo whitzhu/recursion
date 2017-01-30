a24c007bbbddd24ea14e75f4fb5eadf28e22fe38
whitzhu <whitzhu@gmail.com>
2017-01-30 10:44:32 -0800

    completed assignment

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,40 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className,originalName, answer) {
+	
+	if(typeof className == 'string'){
+		var answer = []
+		originalName = className.substr(0);
+
+		if(document.body.className == className){
+			
+			answer.push(document.body)
+			
+			if ( getElementsByClassName(document.body, originalName, answer) !== undefined){
+				answer.push(getElementsByClassName(document.body, originalName, answer));
+			}
+			return answer;
+			
+		}else{
+			answer.push(getElementsByClassName(document.body, originalName, answer));
+		}
+	
+	}else{
+
+		var child = className.childNodes;
+		for (var i = 0 ; i < child.length; i++){
+			if(child[i].childNodes.length >= 0 ){
+				getElementsByClassName(child[i], originalName, answer);
+			}
+
+			if(typeof child[i].className == 'string'){
+				
+				if(child[i].className.indexOf( originalName) !== -1 || child[i].className == originalName ){
+					answer.push(child[i]);
+				}
+			}
+		}
+	}
+
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,52 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+
+	function isEmpty(obj){
+		for (var key in obj){
+			if(obj.hasOwnProperty(key)){
+				return true;
+			}
+		}
+		return false;
+	}
+
+	if (typeof obj == "number") {
+		return obj.toString();
+	}else if ( obj === null ){
+		return "null";
+	}else if( obj === true ){
+		return "true";
+	}else if( obj === false ){
+		return "false";
+	}else if (typeof obj == "string"){
+		return '"'+ obj + '"';
+	
+	}else if (Array.isArray(obj) ){
+		var newArray = [];
+
+		if (obj.length == 0){
+			return '['+ obj +']';
+			
+		}else{
+			for (var i = 0; i < obj.length; i++ ){
+				newArray.push(stringifyJSON(obj[i]));
+		
+			}
+			return '['+ newArray.join(',') +']';
+		}
+	}else{
+		var newObj= [];
+		if(!isEmpty(obj)){
+			return '{' + '}';
+		}
+		for(var key in obj){
+			if (typeof obj[key] == "undefined"){
+				return '{' + '}';
+			}
+			newObj.push(stringifyJSON(key) + ":" + stringifyJSON(obj[key]));
+		}
+		return '{' + newObj  + '}';
+	}
+
 };
